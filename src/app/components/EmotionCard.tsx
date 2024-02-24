04857cd7691604556a31bd24fdee093e35946294
woaitsAryan <yoprince.ab@gmail.com>
2024-02-24 18:33:37 +0530

    feat:basic ui

--- src/app/components/EmotionCard.tsx
+++ src/app/components/EmotionCard.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 const EmotionCard = () => {
   return (
-   <div className="">
-
+   <div className="bg-gray-400 flex flex-col gap-[.5rem] rounded-[1rem] px-[1rem] py-[0.5rem] w-[30%] z-[1000]">
+    <h1 className="text-xl font-bold">User prompt: Lorem ipsum dolor sit amet ...  </h1>
+    <p>Models reply :  Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, neque molestias. Aliquam ad minima cum est. Et qui dicta accusantium.........</p>
+    <p className="w-full text-right font-semibold">27th November,2024</p>
    </div>
   );
 };
--- src/app/emotion/page.tsx
+++ src/app/emotion/page.tsx
@@ -1,10 +1,25 @@
 import EmotionCard from "../components/EmotionCard";
 export default function Emotion() {
   return (
-    <div className="w-full h-screen bg-bg_primary  p-8">
+    <div className="w-full min-h-screen bg-bg_primary flex flex-col justify-center items-center gap-[2rem]  p-8  relative">
       <div className="heading font-mona_bold tab:text-7xl py-2 bg-gradient-to-r from-indigo-700 via-purple-300 to-pink-400 bg-clip-text text-transparent mobile:text-4xl mobile:leading-[3rem] w-full flex justify-center items-center">
         My diary
       </div>
+      <input className=" resize-none text-white px-[1rem] py-[0.5rem] bg-[rgba(255,255,255,.1)] rounded-2xl no-scrollbar w-[30%] focus:border-primary_text break-words whitespace-pre-wrap h-auto leading-[1.75rem]" placeholder="Search"/>
+      <div className="w-full flex flex-wrap gap-[1rem] justify-center items-start">
+        <EmotionCard />
+        <EmotionCard /> 
+        <EmotionCard />
+        <EmotionCard />
+        <EmotionCard />
+        <EmotionCard /> 
+        <EmotionCard />
+        <EmotionCard /> <EmotionCard />
+        <EmotionCard /> 
+        <EmotionCard />
+        <EmotionCard />
+      </div>
+      
     </div>
   );
 }
